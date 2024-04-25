862f27fcdfa0461fe4b2678861db090a1ceb3a12
mhmadrizallll <muhammadrizal087733521@gmail.com>
2024-04-25 21:02:47 +0700

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const kubus = require("./kubus.js");
 const { volumeBalok, luasPermukaanBalok } = require("./balok.js");
 const { luasPermukaanPrisma, volumePrisma } = require("./prisma.js");
+const saveData = require("./people.js");
+const fs = require("fs");
 
 console.log(kubus(8, 12));
 console.log("volume balok = ", volumeBalok(8, 12, 10));
 console.log("luas permukaan balok = ", luasPermukaanBalok(8, 12, 10));
 console.log("volume prisma = ", volumePrisma(8, 12, 10));
 console.log("Luas permukaan prisma = ", luasPermukaanPrisma(8, 12, 10));
+
+fs.writeFileSync(
+  "./result.txt",
+  `Luas permukaan balok = ${luasPermukaanBalok(
+    8,
+    12,
+    10
+  )}, \nVolume balok = ${volumeBalok(8, 12, 10)}`
+);
+
+const data = fs.readFileSync("./result.txt", "utf8");
+console.log(data);
+
+saveData();
